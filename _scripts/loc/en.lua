d783ffc62f99428fba4a31b89b25a69a500db648
andre <andrea.calzari@student.supsi.ch>
2026-05-01 17:27:05 +0200

    TOLTO EXIT E ADESSO STORY E CREDITS FAN QUALCOSA

--- _scripts/loc/en.lua
+++ _scripts/loc/en.lua
@@ -3,7 +3,7 @@ local M = {}
 M.strings = {
 	play = "PLAY",
 	credits = "CREDITS",
-	exit = "EXIT",
+	story = "STORY",
 	label_username = "Username:",
 	label_password = "Password:",
 	insert_user = "Insert Username",
@@ -29,6 +29,7 @@ M.strings = {
 	msg_gameover = "GAME OVER!",
 	button_trash = "TRASH",
 	button_eat = "EAT",
+	story_text = "YETI SUSTAINABILITY PROJECT\nWasted food drains energy from the mountain and disrupts the village's balance.\n\nGAME MECHANICS:\n1. SKI LIFT: Collect coins and Knowledge Points while dodging obstacles.\n2. MOOD SYSTEM: The Yeti's emotional state affects his hunger. If he becomes Furious, the game locks.\n3. MARKET: Buy food from mentors, but watch the expiration dates to avoid waste.\n4. SCHOOL: Consult Professor Owl to get Fun Facts and increase your level.\n\nWork with the mountain mentors to save the village and feed the Yeti sustainably!",
 	school_guide = [[
 	WELCOME TO THE SCHOOL!
 
--- _scripts/loc/it.lua
+++ _scripts/loc/it.lua
@@ -3,7 +3,7 @@ local M = {}
 M.strings = {
 	play = "GIOCA",
 	credits = "CREDITI",
-	exit = "ESCI",
+	story = "STORIA",
 	label_username = "Utente:",
 	label_password = "Password:",
 	insert_user = "Inserisci Utente",
@@ -29,6 +29,7 @@ M.strings = {
 	msg_gameover = "HAI PERSO!",
 	button_trash = "BUTTA",
 	button_eat = "MANGIA",
+	story_text = "YETI SUSTAINABILITY PROJECT\nIl cibo sprecato sottrae energia alla montagna e danneggia l'equilibrio del villaggio.\n\nMECCANICHE DI GIOCO:\n1. SKI LIFT: Raccogli monete e Punti Conoscenza schivando gli ostacoli.\n2. MOOD SYSTEM: Lo stato emotivo del Yeti influenza la sua fame. Se è furioso, il gioco si blocca.\n3. MARKET: Acquista cibo dai mentor, ma attenzione alla data di scadenza per evitare sprechi.\n4. SCHOOL: Consulta Professor Owl per ottenere Fun Facts e aumentare il tuo livello.\n\nCollabora con i mentor della montagna per salvare il villaggio e nutrire il Yeti in modo sostenibile!",
 	school_guide = [[
 	BENVENUTO A SCUOLA!
 
